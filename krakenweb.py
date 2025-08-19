c0e1f2d8580a83a75ee371758b677f8d1461eee8
Jose Alfredo Insfran <jinsfran@criterion.com.py>
2025-08-19 11:41:04 -0300

    Agregar path por defecto

--- krakenweb.py
+++ krakenweb.py
@@ -168,9 +168,7 @@ def get_env_variable(variable_name, default=None):
     
     # Validación específica para FOLDER_PATH
     if variable_name == 'FOLDER_PATH' and not value:
-        print("⚠️  FOLDER_PATH no encontrada en variables de entorno")
         value = "/var/www/html/paginas_archivo/"  # Tu ruta fija aquí
-        print(f"✅ Usando ruta por defecto: {value}")
     
     return value
 
